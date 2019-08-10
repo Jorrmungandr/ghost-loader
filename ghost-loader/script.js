71703ed2ab46e083837802e0beb7c04c0bf1ff97
Yorrmungand <edgarmarques@hotmail.com.br>
2019-08-10 08:20:41 -0300

    Finish bar

--- ghost-loader/script.js
+++ ghost-loader/script.js
@@ -4,18 +4,30 @@ let ghostBarWidth = 0;
 
 const bar = gel('.bar');
 let barWidth = 0;
+const maxWidth = gel('.bar-wrapper').clientWidth;
 
 document.addEventListener('keydown', (event) => {
-  if (event.keyCode === 32 && ghostBar.clientWidth < gel('.bar-wrapper').clientWidth) {
+  if (event.keyCode === 32 && ghostBar.clientWidth < maxWidth) {
     ghostBarWidth = ghostBar.clientWidth + 10;
+    if (ghostBarWidth >= maxWidth) ghostBarWidth = maxWidth;
     ghostBar.style.width = `${ghostBarWidth}px`;
   }
 });
 
 setInterval(() => {
   if (ghostBarWidth > 50) {
-    let factor = ((ghostBarWidth - barWidth) / gel('.bar-wrapper').clientWidth);
+    if (barWidth >= maxWidth) {
+      bar.style.transition = 'width .5s ease-in';
+      bar.style.width = `${maxWidth}px`;
+      return;
+    }
+    let factor = ((ghostBarWidth - barWidth) / maxWidth);
+
+    if (factor < 0.000000001) factor = 0;
+
     barWidth += 120 * factor;
+
+    if (ghostBarWidth >= maxWidth) barWidth = maxWidth;
     bar.style.width = `${barWidth}px`;
   }
-}, 300);
+}, 500);
